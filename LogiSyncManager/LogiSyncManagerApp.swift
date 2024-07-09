79f48c6d4343416315a72728a1a443084584b82e
me990928 <me990928@gmail.com>
2024-07-09 12:09:34 +0900

    Initial Commit

---
+++ LogiSyncManager/LogiSyncManagerApp.swift
@@ -0,0 +1,32 @@
+//
+//  LogiSyncManagerApp.swift
+//  LogiSyncManager
+//
+//  Created by 広瀬友哉 on 2024/07/09.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct LogiSyncManagerApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
