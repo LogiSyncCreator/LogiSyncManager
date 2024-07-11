abf01d2a2146d446c02a0efc165259a99c7a3ad0
me990928 <me990928@gmail.com>
2024-07-11 15:56:13 +0900

    ログイン画面

--- LogiSyncManager/Login/LoginView.swift
+++ LogiSyncManager/Login/LoginView.swift
@@ -8,11 +8,45 @@
 import SwiftUI
 
 struct LoginView: View {
+    
+    @Binding var index: Int
+    
+    @State var userId: String = ""
+    @State var userPass: String = ""
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Rectangle().ignoresSafeArea().overlay {
+        VStack(alignment: .center){
+            Text("logisync").textCase(.uppercase).font(.system(size: 80)).bold().foregroundColor(Color(.label))
+            Group{
+                TextField("USERID", text: $userId).font(.title).foregroundColor(Color(.label))
+                Divider()
+                SecureField("PASSWORD", text: $userPass).font(.title).foregroundColor(Color(.label))
+                Divider()
+            }.frame(width: 700)
+            HStack(content: {
+                Button(action: {
+                    
+                }, label: {
+                    Text("新規登録").foregroundStyle(.white).font(.title).padding(EdgeInsets(top: 5, leading: 10, bottom: 5, trailing: 10)).bold()
+                }).background(.blue, in: RoundedRectangle(cornerRadius: 5)).padding(.trailing)
+                Button(action: {
+                    // ログイン処理
+                    
+                    withAnimation {
+                        index = 1
+                    }
+                    
+                    
+                }, label: {
+                    Text("ログイン").foregroundStyle(.white).font(.title).padding(EdgeInsets(top: 5, leading: 10, bottom: 5, trailing: 10)).bold()
+                }).background(.blue, in: RoundedRectangle(cornerRadius: 5)).padding(.leading)
+            }).padding()
+        }.padding()
+    }.foregroundStyle(.background)
     }
 }
 
 #Preview {
-    LoginView()
+    ContentView()
 }
